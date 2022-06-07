070a8ad1e7382791ebf8da31ea67c18da3840b19
Floris <58078572+Flor0s@users.noreply.github.com>
2022-06-07 15:14:14 +0200

    particle animation
    Added particle for confetti
    Added Animation for grow shrink
    Added That most scripts find what they need

--- Assets/Scripts/ThermoLqued.cs
+++ Assets/Scripts/ThermoLqued.cs
@@ -7,7 +7,6 @@ public class ThermoLqued : MonoBehaviour
     public bool allBuildingsBuilt = false;//If this is true, tentacles must light up
     public bool ThermometerFull = false;// if this is true player lost
     public float GrowAmount;//amount that the temprature rises
-    public float ShrinkAmount;//amount that the temprature goes down
     public float timer;
     private float timeLeft;
 
@@ -36,9 +35,9 @@ public class ThermoLqued : MonoBehaviour
         }
     }
 
-    public void CO2Down()
+    public void CO2Down(float Co2loweramount)
     {
-        gameObject.transform.localScale -= new Vector3(0, ShrinkAmount, 0);
+        gameObject.transform.localScale -= new Vector3(0, Co2loweramount, 0);
     }
 
     private void resetTimer()
